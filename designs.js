d8788244353268588c63875bb8b14794be37b29a
Jarvis Smith <jarvis_smith@charter.net>
2017-11-14 15:51:18 -0800

    Made many changes - too many for one commit, sorry!

--- designs.js
+++ designs.js
@@ -1,15 +1,12 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 //Define global variables
 let gridzoom, gridheight, gridwidth, colIndex, rowIndex, pxcolor;
 let x, y, bg;
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
-//Create the makeGrid function
+//Following function draws the editor table
 function makeGrid() {
+    //Clear table
+    $('#pixel_canvas').empty();
     //for next loops to draw the rows and colums in the table
     for (y = 0; y < gridheight; y++) {
         $('#pixel_canvas').append('<tr>');
@@ -17,18 +14,15 @@ function makeGrid() {
             $('#pixel_canvas > tr').last().append('<td>');
         }
     }
-    //Create canvas to draw preview in and fill it with the background color
-    $('#myCanvas').attr('width', gridwidth).attr('height', gridheight);
-    pxcolor = document.getElementById("colorPicker").value;
-    ctx.fillStyle = pxcolor;
-    ctx.fillRect(0, 0, gridwidth, gridheight);
 }
-
 //This code makes sure that I get hex values from the table tr td
+function hex(x) {
+    return ("0" + parseInt(x).toString(16)).slice(-2);
+}
 $.cssHooks.backgroundColor = {
     get: function (elem) {
         if (elem.currentStyle) {
-            bg = elem.currentStyle["backgroundColor"];
+            bg = elem.currentStyle.backgroundColor;
         } else if (window.getComputedStyle) {
             bg = document.defaultView.getComputedStyle(elem,
                 null).getPropertyValue("background-color");
@@ -37,58 +31,70 @@ $.cssHooks.backgroundColor = {
             return bg;
         } else {
             bg = bg.match(/\d+/g);
-            function hex(x) {
-                return ("0" + parseInt(x).toString(16)).slice(-2);
-            }
             return "#" + hex(bg[0]) + hex(bg[1]) + hex(bg[2]);
         }
     }
-}
-
+};
 //Change gridzoom if slider is adjusted
-$('[type="range"]').mouseup(function(){
-    gridzoom = document.getElementById("slideContainer").value;
+$('#slideContainer').mouseup(function(){
+    gridzoom = $(this).val();
     $('tr').css('height', gridzoom + 'px');
     $('td').css('width', gridzoom + 'px');
 });
-
-//set pxcolor from the color picker upon choice
-$('#colorPicker').mouseout(function(){
-    pxcolor = document.getElementById("colorPicker").value;
-});
-
-//Clear table, set gridheight and gridwidth from input fields call makegrid
-//set gridzoom from slider, set css valuse for tr height and td width
-//set pxcolor from colorPicker, set all tr and td to background color
-//unhide span elements
-$('[type="submit"]').click(function(){
-    $('#pixel_canvas').empty();
+//Detect the click of the submit button
+$('#submit_button').click(function(){
+    //Set gridheight and gridwidth
     gridheight = $('#input_height').val().valueOf();
     gridwidth = $('#input_width').val().valueOf();
+    //Call function to draw grid
     makeGrid();
-    gridzoom = document.getElementById("slideContainer").value;
+    //Set color of all table elements to white
+    $('#pixel_canvas > tr, td').css('background-color', '#ffffff');
+    //Set canvas dimensions
+    $('#myCanvas').attr('width', gridwidth).attr('height', gridheight);
+    //Set initial zoom value for the table
+    gridzoom = $('#slideContainer').val();
     $('tr').css('height', gridzoom + 'px');
     $('td').css('width', gridzoom + 'px');
-    pxcolor = document.getElementById("colorPicker").value;
-    $('tr td').css({backgroundColor: pxcolor});
+    //Unhide on-screen help text
     $('span').css('visibility', 'visible');
+    //Unhide the clear canvas button
+    $('#clear_color').css('visibility', 'visible');
 });
-
-//Check for click within table and determine row and colum of clicked td
-//set clicked td to pxcolor
-//draw pixel on the preview canvas
+//Clear table and redraw to current color
+$('#clear_color').click(function(){
+    //Call function to draw grid
+    makeGrid();
+    //Check zoom value and assign css values
+    gridzoom = $('#slideContainer').val();
+    $('tr').css('height', gridzoom + 'px');
+    $('td').css('width', gridzoom + 'px');
+    //Set all tr and td elements to pxcolor
+    $('tr, td').css('backgroundColor', pxcolor);
+    //Fill preview canvas with pxcolor
+    ctx.fillStyle = pxcolor;
+    ctx.fillRect(0, 0, gridwidth, gridheight);
+});
+//Check for click within table
 $('#pixel_canvas').on('click','td', function(){
+    //Get colIndex and rowIndex values
     colIndex = parseInt( $(this).index() ) + 1;
     rowIndex = parseInt( $(this).parent().index() )+1;
+    //Make sure we have pxcolor set from color picker
+    pxcolor = $('#colorPicker').val();
+    //Change the css background color of the clicked element
     $('tr:nth-child(' + rowIndex + ') > td:nth-child(' + colIndex + ')').css({backgroundColor: pxcolor});
+    //Draw pixel on preview canvas
     ctx.fillStyle = pxcolor;
     ctx.fillRect( colIndex - 1 , rowIndex - 1, 1, 1 );
 });
-
-//detect right click on td and set pxcolor and picker to that color
+//detect right click on table element, set pxcolor and picker to the existing color
 $('#pixel_canvas').on('auxclick','td', function(){
+    //Get colIndex and rowIndex values
     colIndex = parseInt( $(this).index() ) + 1;
     rowIndex = parseInt( $(this).parent().index() )+1;
+    //Get existing css background color of the auxclicked element
     pxcolor = $('tr:nth-child(' + rowIndex + ') > td:nth-child(' + colIndex + ')').css('background-color');
+    //Set the color picker to the color stored in pxcolor
     $('#colorPicker').val(pxcolor);
 });
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # Pixel Art Maker Project
 
 ## Instructions
-Open index.html in a browser of your choice.  Enter desired grid height and grid width into the input fields, select an intial color for the background and click submit.  Clicking the submit button will clear the grid and start from scratch.
-Then slider control can be used to change your zoom level at anytime during editing.
+Open index.html in a browser of your choice.  Enter desired grid height and grid width into the input fields.  Clicking the submit button will clear the grid and start from scratch.
+The slider control can be used to change your zoom level at anytime during editing.
 Right click on previous plotted pixels to set brush color to the color originaly used.
 Right click on the preview image and choose Save Image as to create a .png file localy.
-
+Click on 'Clear Canvas' to clear and set background color of entire area.
